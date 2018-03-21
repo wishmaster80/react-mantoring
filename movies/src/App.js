872da104578eb34769fdb2e95500473fb5159253
wishmaster80 <wishmaster80@o2.pl>
2018-03-21 14:57:28 +0100

    code refactoring

--- movies/src/App.js
+++ movies/src/App.js
@@ -4,38 +4,32 @@ import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    //debugger;
-    this.state = {
+  state = {
     movies: [],
     activeMovieId: false
-    }
-    this.handler = this.handler.bind(this)
   }
 
-    handler = (movieId) => {
-    this.setState({activeMovieId: movieId}); 
-    }
+  handler = (movieId) => {
+    this.setState({ activeMovieId: movieId });
+  }
 
-  async componentDidMount() {    
-    const movies =  await this.GetMovies();    
-    this.setState({movies : movies})
+  async componentDidMount() {
+    const movies = await this.GetMovies();
+    this.setState({ movies: movies })
   }
   async GetMovies() {
     const response = await fetch('https://react-mentoring-backend.herokuapp.com/movies')
     return await response.json()
   }
-  
+
   listMovie(movies) {
-    return movies.map( (movie) =>  <Movie key={movie.id} id={movie.id} movie={movie} handler={this.handler} isToggleOn={this.state.activeMovieId == movie.id} />   )
-  } 
+    return movies.map((movie) => <Movie key={movie.id} id={movie.id} movie={movie} handler={this.handler} isToggleOn={this.state.activeMovieId == movie.id} />)
+  }
   render() {
-      console.log('render' + this.state.activeMovieId)
-    return (      
-      <ul>        
-          {this.listMovie(this.state.movies)}
+    console.log('render' + this.state.activeMovieId)
+    return (
+      <ul>
+        {this.listMovie(this.state.movies)}
       </ul>
     );
   }
--- movies/src/Movie.js
+++ movies/src/Movie.js
@@ -3,72 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 
 class Movie extends Component {
-    constructor(props) {
-        super(props);
-        //debugger;
-        this.movie = props.movie;
-        this.state = {
-            detail: [],
-            item: []            
-          }
-          this.isToggleOn = false
-       this.handleClick = this.handleClick.bind(this);
-      }    
-         
-      handleClick() {
-       this.props.handler( this.isToggleOn ? 0 : this.props.id);
-      }
+  state = {
+    detail: [],
+    item: []
+  }
+  handleClick = () => {
+    this.props.handler(this.props.isToggleOn ? 0 : this.props.id);
+  }
 
-    componentDidMount() {
-        this.setState({detail:
-          <div>  
+  componentDidMount() {
+    this.setState({
+      detail:
+        <div>
+          <div>
             <div>
-            <div>
-              {this.movie.genres.reduce((prev, curr) => [...prev, ', ', curr])}
+              {this.props.movie.genres.reduce((prev, curr) => [...prev, ', ', curr])}
             </div>
             <div>
-              {this.movie.actors.reduce((prev, curr) => [...prev, ', ', curr])}
-            </div>                          
-              <img
-                className="Avatar"
-                src={this.movie.posterurl}                
-              />
+              {this.props.movie.actors.reduce((prev, curr) => [...prev, ', ', curr])}
             </div>
+            <img
+              className="Avatar"
+              src={this.props.movie.posterurl}
+            />
           </div>
-            })
+        </div>
+    })
 
-            this.setState({info:                
-              <div>
-              <div>
-                {this.movie.title}
-              </div>
-              <div>
-                {this.movie.year}
-              </div>
-              <div>                
-                {(this.movie.ratings.reduce((a, b) => (a + b)/this.movie.ratings.length).toFixed(2)) }
-              </div>
-              </div>
-              })
-}
+    this.setState({
+      info:
+        <div>
+          <div>
+            {this.props.movie.title}
+          </div>
+          <div>
+            {this.props.movie.year}
+          </div>
+          <div>
+            {(this.props.movie.ratings.reduce((a, b) => (a + b) / this.props.movie.ratings.length).toFixed(2))}
+          </div>
+        </div>
+    })
+  }
 
-  
+
   render() {
-    this.isToggleOn = this.props.isToggleOn;
-    console.log(this.movie.id)    
     return (
-      
-        <div>          
-          {this.state.info}
-            <button onClick={this.handleClick}>            
-           
-            {this.isToggleOn ? 'hide' : 'show'}
-      </button>
-      {this.isToggleOn ? this.state.detail : ''}
-      
+
+      <div>
+        {this.state.info}
+        <button onClick={this.handleClick}>
+
+          {this.props.isToggleOn ? 'hide' : 'show'}
+        </button>
+        {this.props.isToggleOn ? this.state.detail : ''}
+
       </div>
     );
-  }  
+  }
 
 }
 
