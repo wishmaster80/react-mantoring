2f4dcfedf527304f146235527a8ad43660e78ba1
wishmaster80 <wishmaster80@o2.pl>
2018-03-27 10:37:55 +0200

    movie details

--- movies/src/MovieDetails.js
+++ movies/src/MovieDetails.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './MovieDetails.css';
+class MovieDetails extends Component {
+  state = {
+    movie: {},
+  }
+  async componentDidMount() {
+    const movie = await this.GetMovie();
+    this.setState({ movie: movie })
+  }
 
-const MovieDetails = (props) => {
-  return (
-    <div className="container">
-      <h1>Details!! {props.match.params.id}</h1>
-    </div>
-  );
+  async GetMovie() {
+    var url = 'https://react-mentoring-backend.herokuapp.com/movies/' + this.props.match.params.id
+    const response = await fetch(url)
+    return await response.json()
+  }
+  render() {
+    return (
+      <div  class="row">
+        <div class="col">
+          <img            
+            src={this.state.movie.posterurl}
+          />
+        </div>
+        <div class="col">
+          <div>{this.state.movie.title}</div>
+          { <div>
+            {this.state.movie.genres && this.state.movie.genres.reduce((prev, curr) => [...prev, ', ', curr])}
+          </div> }
+          { <div>
+            {this.state.movie.actors && this.state.movie.actors.reduce((prev, curr) => [...prev, ', ', curr])}
+          </div> }
+          <div>
+            {this.state.movie.year}
+          </div>
+          <div>
+            {this.state.movie.ratings && (this.state.movie.ratings.reduce((a, b) => (a + b) / this.state.movie.ratings.length).toFixed(2))}
+          </div>
+          <div>
+            {this.state.movie.storyline}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default MovieDetails;
