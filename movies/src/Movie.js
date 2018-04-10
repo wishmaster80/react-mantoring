83f0615243dec8d8adc2c91d8b413e3ec1c7aec5
wishmaster80 <wishmaster80@o2.pl>
2018-04-10 14:35:08 +0200

    React.createContext();

--- movies/src/Movie.js
+++ movies/src/Movie.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import { Link } from 'react-router-dom';
+
 
 class Movie extends Component {
   constructor(props) {
--- movies/src/MovieFeatured.js
+++ movies/src/MovieFeatured.js
@@ -1,70 +1,85 @@
 import React, { Component } from 'react';
 import './App.css';
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FeaturedContext } from './Featured'
+
 
 class Movie extends Component {
   state = {
     detail: [],
     item: []
   }
-  componentDidMount() {
-    this.setState({
-      detail:
-        <div>
-          <div>
-            <div>
-              {this.props.movie.genres.reduce((prev, curr) => [...prev, ', ', curr])}
-            </div>
-            <div>
-              {this.props.movie.actors.reduce((prev, curr) => [...prev, ', ', curr])}
-            </div>
-            <img
-              className="Avatar"
-              src={this.props.movie.posterurl}
-              alt={this.state.movie.title}
-            />
-          </div>
-        </div>
-    })
 
-    this.setState({
-      info:
-        <div>
-          <div>
-            {this.props.movie.title}
-          </div>
-          <div>
-            {this.props.movie.year}
-          </div>
-          <div>
-            {(this.props.movie.ratings.reduce((a, b) => (a + b) / this.props.movie.ratings.length).toFixed(2))}
-          </div>
-          <div>
-            {this.props.movie.imdbRating}
-          </div>          
-          <div>
-              <Link to={`/movie/${this.props.id}`}>Movie details</Link>
-          </div>          
-        </div>
-    })
+  info(context) {
+    return <MovieInfo movie={context.state.movies[this.props.id]} />
   }
-  infoPart =()=>{
-    return (this.state.info)
-  }
-  conditionalDetailPart() {
-    return (this.state.detail);
+  detailPart(context) {
+    return <MovieDetails movie={context.state.movies[this.props.id]} />
   }
 
   render() {
     return (
-      <div>
-        {this.infoPart()}        
-        {this.props.isToggleOn && this.conditionalDetailPart()}
-
-      </div>
+      <FeaturedContext.Consumer>
+        {(context) => (
+          <React.Fragment>
+            {this.info(context)}
+            {this.detailPart(context)}
+          </React.Fragment>
+        )}
+      </FeaturedContext.Consumer>
     );
   }
 
 }
 
 export default Movie;
+
+class MovieInfo extends Component {
+
+  render() {
+    if (this.props.movie === undefined) return (<div> movie is undefined </div>)
+    return (
+      <div>
+        <div>
+          <div>
+
+            {this.props.movie.genres.reduce((prev, curr) => [...prev, ', ', curr])}
+          </div>
+          <div>
+            {this.props.movie.actors.reduce((prev, curr) => [...prev, ', ', curr])}
+          </div>
+          <img
+            className="Avatar"
+            src={this.props.movie.posterurl}
+            alt={this.props.movie.title}
+          />
+        </div>
+      </div>);
+  }
+}
+
+
+class MovieDetails extends Component {
+  render() {
+    if (this.props.movie === undefined) return (<div> movie is undefined </div>)
+    return (
+      <div>
+        <div>
+          {this.props.movie.title}
+        </div>
+        <div>
+          {this.props.movie.year}
+        </div>
+        <div>
+          {(this.props.movie.ratings.reduce((a, b) => (a + b) / this.props.movie.ratings.length).toFixed(2))}
+        </div>
+        <div>
+          {this.props.movie.imdbRating}
+        </div>
+        <div>
+          <Link to={`/movie/${this.props.id}`}>Movie details</Link>
+        </div>
+        <div>-------------------------------------------------------------</div>
+      </div>);
+  }
+}
